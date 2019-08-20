ea114aaba16e381f96a1720921b587e4ab5f3d63
Sunil Limbu <sunil@sunil.com>
2019-08-20 22:27:37 +0800

    added travis config for aws codedeploy

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,3 @@ RUN npm run export
 FROM nginx
 EXPOSE 80
 COPY --from=builder /app/out /usr/share/nginx/html
-COPY --from=builder /app/.next /usr/share/nginx
-
--- docker-start.sh
+++ docker-start.sh
@@ -2,5 +2,6 @@
 yum install -y htop
 cd /tmp
 docker build -t writer-kata .
-docker run writer-kata
+docker run -p 80:80 writer-kata
+docker system prune
 # docker build -t writer-kata -f /tmp/Dockerfile .
