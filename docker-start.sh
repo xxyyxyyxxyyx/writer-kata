7e77dba4db7916f33b0fb153dfa9952823dd80da
Sunil Limbu <sunil@sunil.com>
2019-08-20 22:46:44 +0800

    added docker config

--- docker-start.sh
+++ docker-start.sh
@@ -3,5 +3,5 @@ yum install -y htop
 cd /tmp
 docker build -t writer-kata .
 docker run -d -p 80:80 writer-kata
-docker system prune
+docker system prune -f
 # docker build -t writer-kata -f /tmp/Dockerfile .
