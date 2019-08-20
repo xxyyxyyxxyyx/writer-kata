dcddd85e59c211b9b43954e92be4bf3c77cce4ea
Sunil Limbu <sunil@sunil.com>
2019-08-20 22:38:31 +0800

    added travis config for aws codedeploy

--- docker-start.sh
+++ docker-start.sh
@@ -2,6 +2,6 @@
 yum install -y htop
 cd /tmp
 docker build -t writer-kata .
-docker run -p 80:80 writer-kata
+docker run -d -p 80:80 writer-kata
 docker system prune
 # docker build -t writer-kata -f /tmp/Dockerfile .
